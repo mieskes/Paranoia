1f9fd890c0f3fee9f51b2d09e1674fcb55cc92bb
Durzo <b_buedenbender@yahoo.de>
2018-04-23 22:36:31 +0200

    Updated to do

--- AcousticFeatures/main.py
+++ AcousticFeatures/main.py
@@ -1,14 +1,14 @@
 """
 Written by Bjoern Buedenbender, 2018
 first steps into getting Acoustic Features from Therapists Recorded Audio sessions
-  -Get a Git Identity (already have an Account?)
   -Consider Class Structure
   -Check the Docs for pympi (getSegment or similar Function Avaiable?)
   -Extract Annotations (Segments) from Elan Files
     -Write a Function which retrives the Timestamps of the Therapists
   -Get a Loop Running which Calculates the FeatureSets (pyAudioAnalysis)
    for the Therapists Segments
-  -Write Output into a .txt file
+  -Write Output into a .arff file
+    -Details about the .arff file Format
 """
 
 def main():
