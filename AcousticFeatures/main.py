71c9b7f0663a50189071bdc37f994f9771ce246c
Durzo <b_buedenbender@yahoo.de>
2018-04-23 13:10:34 +0200

    Starting point for acoustic feature extraction

---
+++ AcousticFeatures/main.py
@@ -0,0 +1,18 @@
+"""
+Written by Bjoern Buedenbender, 2018
+first steps into getting Acoustic Features from Therapists Recorded Audio sessions
+  -Get a Git Identity (already have an Account?)
+  -Consider Class Structure
+  -Check the Docs for pympi (getSegment or similar Function Avaiable?)
+  -Extract Annotations (Segments) from Elan Files
+    -Write a Function which retrives the Timestamps of the Therapists
+  -Get a Loop Running which Calculates the FeatureSets (pyAudioAnalysis)
+   for the Therapists Segments
+  -Write Output into a .txt file
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
