03dd29ba6d16c19c10743aeddd5d840ee2c957dc
Durzo <b_buedenbender@yahoo.de>
2018-05-02 18:28:45 +0200

    Added a parser for the segment txt files

--- AcousticFeatures/main.py
+++ AcousticFeatures/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import sys
 import os
 # sys.pasth.append(os.path.abspath("pyAudio"))
@@ -8,7 +9,8 @@ from pyAudioAnalysis import audioFeatureExtraction as ftExt
 
 
 """Written by Bjoern Buedenbender, 2018
-first steps into getting Acoustic Features from Therapists Recorded Audio sessions"""
+Dependencies:
+    -pyAudioAnalysis"""
 #TODO: Write Java Function in ClassfiyTabReader that gives a Txt File with the Audio name and TimeStamp of the Segments
 #TODO: Write a Function which retrives the Timestamps of the Therapists
 #TODO: Get a Loop Running which Calculates the FeatureSets (pyAudioAnalysis) for the Therapists Segments
@@ -37,9 +39,62 @@ class AcousticFeatureExtractor:
                 if isFileOfFormat(singleAudioFile,self.AUDIO):
                     self.audioFiles.append(singleAudioFile)
 
-    def parseSegmentFiles(self):
-        pass
+    def parseSegmentFile(self,singleSegmentFile,parseSequence="Therapeut\tTherapeut"):
+        """Takes a .txt file and parseSquence which denotes the starting point
+        of a Segment and returns a List of the Segments in the format of [Beginn] [End] [Duration]
+        the function additionally returns the path to the matching audio file if specified in the first
+        line of the txt file.
+
+        Parameters
+        ----------
+        singleSegmentFile : String
+            Path to the segment txt file.
+        parseSequence : String
+            Substring that indicates the start of a segment for example \"Therapeut\tTherapeut\".
+
+        Returns
+        -------
+        List
+            List containing all Segments in three timestamps.
+        String
+            Optional: Link to a referenced audio file
+
+        """
+        """GENERAL ASSUMPTIONS for this parser to work
+                -timestamps are segmented by the tabulator
+                -txt file contains the parseSequence"""
+        listOfSegments = [] #Format: Timestamps of [Beginn] [End] [Duration]
+        audioFileInSegmentTxt = ""
+        lengthOfParseSequence = len(parseSequence)
+
+        with open(singleSegmentFile,"r") as f:
+            #Checks if the Location to the AudioFiles is Present saves it in local var
+            firstLine = f.readline()
+            if (firstLine[:1] == "[") and (firstLine[-2:-1]=="]"):
+                linkToAudioFile = firstLine[1:-2] #Removing the Parser Seq [ and ] from string
+                if isFileOfFormat(linkToAudioFile,self.AUDIO):
+                    audioFileInSegmentTxt = linkToAudioFile
+            for line in f:
+                if line.find(parseSequence) != -1: #Checks if the Name segment is present in the current line
+                    wordsOfLine = line[lengthOfParseSequence:].split("\t") #Seperates the timestamps by tabulator
+                    listOfSegments.append((wordsOfLine[1],wordsOfLine[3],wordsOfLine[5]))
+        return listOfSegments,audioFileInSegmentTxt
+
+
+    def calculateAccousticFeatures(self):
+        for counter,singleSegmentFile in enumerate(self.segmentFiles):
+            segmentTimeStamps, audioFile = self.parseSegmentFile(singleSegmentFile)
+            if audioFile == "" and counter <= len(self.audioFiles): #When in the segment txt no audiofile is linked check if a list of audiofiles is given by commandline
+                audioFile = self.audioFiles[counter]
+            if audioFile != "":
+                    [Fs, x] = audioBasicIO.readAudioFile(audioFile);
+                    #Fs = FrameRate, x = Signal
+                    features = ftExt.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs)
+                    print features
+
 #============================================================
+
+
 
 #Initialize a arguments parser for commandline based execution of the Script
 parser = argparse.ArgumentParser(prog='AcousticFeatureExtraction',
@@ -64,14 +119,15 @@ parser.add_argument('--version', action='version', version='%(prog)s 1.1\n'
 #TODO: Consider the Creation of a logfile for the script, or look for a library that does it
 #TODO: Add Parser Argument for a Txt File Containing Links to the segmentTxt and the regarding audio
 #TODO: Write a Reader Function for the LinkTxtFile (Paths to segment and audio files)
+#TODO: Add an Argument for command line Based Execution of the Script like -x (execution)
 
 
 def main(segFiles=None,audFiles=None):
     AcFtEx = AcousticFeatureExtractor(segFiles,audFiles)
+    AcFtEx.calculateAccousticFeatures() #DEBUGGIN Purposes TODO DELETE
 
 
 def isFileOfFormat(filePath,fileFormat,throwError=False):
-
     #Checks if the Parameter fileFormat is Category and contains Multiple Fileformats
     #Iterates threw the Category (list)
     if isinstance(fileFormat,list) or isinstance(fileFormat,tuple):
@@ -92,7 +148,7 @@ def isFileOfFormat(filePath,fileFormat,throwError=False):
             if throwError == True:
                 raise IOError("Couldnt Find File or wasnt a %s File: \"%s\"" % (fileFormat,filePath))
             else:
-                "Couldnt Find File or wasnt a %s File: \"%s\"" % (fileFormat,filePath)
+                print "Couldnt Find File or wasnt a %s File: \"%s\"" % (fileFormat,filePath)
                 return False
 
 if __name__ == "__main__":
