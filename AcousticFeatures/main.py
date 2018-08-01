dd475f992384da4940fc12a9544b4af4405fb6da
Björn Büdenbender <b_buedenbender@yahoo.de>
2018-08-01 19:41:22 +0200

    Added the SMILExtract call, script is now able to extract features and save them to .arff. Also improved the directory and file management

--- AcousticFeatures/main.py
+++ AcousticFeatures/main.py
@@ -7,10 +7,9 @@ from shutil import rmtree
 
 """Written by Bjoern Buedenbender, 2018
 Dependencies:
-    -pyDub"""
-#TODO: Write Java Function in ClassfiyTabReader that gives a Txt File with the Audio name and TimeStamp of the Segments
-#TODO: Write Output into a .arff file
-#TODO: Implement check if cutted wav files already exist
+    -pyDub
+    -openSMILE (/usr/local/bin/SMILExtract)"""
+#TODO: Add an overwrite parameter (if the already existing slices shouldnt be used)
 #TODO: Change pyLinter snake_case naming convention for variables
 
 class AcousticFeatureExtractor(object):
@@ -40,7 +39,7 @@ class AcousticFeatureExtractor(object):
         cutted slices of the .wav files), Hint: improves performance in
         future execution of the script.
     """
-
+    #LISTS containing paths to files
     audioFiles = []     #List in .txt Format with the Audio files
     segmentFiles = []    #List of .txt File for the Timestamp of the Segments
     convertedAudioFiles = [] #List of Files that got converted
@@ -49,11 +48,17 @@ class AcousticFeatureExtractor(object):
     TXT = ".txt"
     AUDIO = (".wav", ".mp3")
     KEEP = True # Keep the cutted wav files for future use
+    THERAPIST = True #True extracts Features for Therapist, False for Patient
+    SPEAKER = "T"
+
     slicesDir = ""
+    featuresDir = ""
     namingOfSlices = 1
     parseSequence="Therapeut\tTherapeut"
+    #[0] = Config for opensmile [1] = Inputfile [2] = Outputfile
+    openSMILEsettings = ["-C OSconfig/IS12_speaker_trait.conf","",""]
 
-    def __init__(self, txtFiles=None, audioFiles=None):
+    def __init__(self, txtFiles=None, audioFiles=None,therapist=True):
         if txtFiles != None:
             for singleTxtFile in txtFiles:
                 if isFileOfFormat(singleTxtFile,self.TXT):
@@ -63,6 +68,14 @@ class AcousticFeatureExtractor(object):
             for singleAudioFile in audioFiles:
                 if isFileOfFormat(singleAudioFile, self.AUDIO):
                     self.audioFiles.append(singleAudioFile)
+
+        self.THERAPIST = therapist #Setting selection of speaker to extract ft from
+        if self.THERAPIST:
+            self.parseSequence = "Therapeut\tTherapeut"
+            self.SPEAKER = "T"
+        else:
+            self.parseSequence = "Patient\tPatient"
+            self.SPEAKER = "P"
 
     def parseSegmentFile(self,singleSegmentFile):
         """Takes a .txt file and parseSquence which denotes the starting point
@@ -115,16 +128,24 @@ class AcousticFeatureExtractor(object):
             rmtree(self.slicesDir)
             print "Deleted all slices (of the original audio file)"
 
-    def extractFeatures(self):
-        pass
-        #
-        # [Fs, x] = audioBasicIO.readAudioFile(tmpPathSoundSlice)
-        # # #Fs = FrameRate, x = Signal
-        # # print Fs
-        # # print x
-        # #TODO: https://github.com/tyiannak/pyAudioAnalysis/issues/18 Possible Solution (Call Stereo to Mono)
-        # features = ftExt.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs)
-        # print features
+    def extractFeatures(self,inputFile):
+        self.openSMILEsettings[1] = "-I " + inputFile
+        cmd_template = 'SMILExtract {config_path} {wav_path} {arff_path}'
+
+        # os.system(cmd_template.format(
+        #     config_path=self.openSMILEsettings[0],
+        #     wav_path=self.openSMILEsettings[1],
+        #     arff_path=self.openSMILEsettings[2],
+        #     ))
+
+    def setNamesOfSlices(self,startInMS,endInMS,i):
+        if self.namingOfSlices == 1:
+            sliceName = self.slicesDir+"/%s-segment%s.wav" %(self.SPEAKER,str(i).zfill(4))
+        elif self.namingOfSlices == 2:
+            sliceName = self.slicesDir+"/%s-segment%s.wav" %(self.SPEAKER,str(str2ms(startInMS)))
+        elif self.namingOfSlices == 3:
+            sliceName = self.slicesDir+"/%s-segment%s-%s.wav" %(self.SPEAKER,str(str2ms(startInMS)),str2ms(endInMS))
+        return sliceName
 
     def getSpeechOfOneSpeaker(self):
         for counter,singleSegmentFile in enumerate(self.segmentFiles):
@@ -139,8 +160,16 @@ class AcousticFeatureExtractor(object):
                     audioFile = self.convertToWav(audioFile)
                     self.convertedAudioFiles.append(audioFile)
 
-                self.slicesDir = os.path.splitext(audioFile)[0] #Directory for slices of the wav file
+                #Directory for slices of the wav file
+                self.slicesDir = os.path.splitext(audioFile)[0] +"_slicesDir"
                 soundBuffer = AudioSegment.from_file(audioFile,format=audioFile[-3:])
+                #Create a Directory for all feature files (.arff)
+                self.featuresDir = os.path.dirname(audioFile) + "/ExtractedFeatures"
+                if not os.path.exists(self.featuresDir):
+                    os.makedirs(self.featuresDir)
+                #sets the outputname for .arff file containing the extracted features
+                outputFile = self.featuresDir + "/AcFt_" +os.path.basename(audioFile)[0:-3] + "arff"
+                self.openSMILEsettings[2] =  "-O " + outputFile
 
                 for i,singleSegment in enumerate(segmentTimeStamps):
                     tmpSoundSlice = soundBuffer[str2ms(singleSegment[0]):str2ms(singleSegment[1])]
@@ -148,20 +177,18 @@ class AcousticFeatureExtractor(object):
                     if not os.path.exists(self.slicesDir):
                         #Create a Directory for all cutted segments of the wav file
                         os.makedirs(self.slicesDir)
-                    if self.namingOfSlices == 1:
-                        tmpPathSoundSlice = os.path.splitext(audioFile)[0]+"/segment%s.wav" %str(i).zfill(4)
-                    elif self.namingOfSlices == 2:
-                        tmpPathSoundSlice = os.path.splitext(audioFile)[0]+"/segment%s.wav" %str(str2ms(singleSegment[0]))
-                    elif self.namingOfSlices == 3:
-                        tmpPathSoundSlice = os.path.splitext(audioFile)[0]+"/segment%s-%s.wav" %(str(str2ms(singleSegment[0])),str2ms(singleSegment[1]))
+                    tmpPathSoundSlice = self.setNamesOfSlices(singleSegment[0],singleSegment[1],i)
                     if not isFileOfFormat(tmpPathSoundSlice,".wav"): #TODO: Consider ADD an Overwrite Parameter
                         fileHandle = tmpSoundSlice.export(tmpPathSoundSlice, format="wav")
+                        self.extractFeatures(tmpPathSoundSlice)
+                    # self.extractFeatures("/media/durzo/69FDAA69060E624F/Uni/Promotion/Aufnahmen/02003_Therapiesitzung22und23_141216_slicesDir/T-segment0000.wav")
 
     def executeFtExtraction(self):
         self.getSpeechOfOneSpeaker()
 
-#============================================================
+#=======================END OF CLASS===========================================
 
+#=====================Utility functions========================================
 def str2ms(s):
     hr, mm, sec = map(float, s.split(':'))
     inMs = ((hr * 60 + mm) * 60 + sec) * 1000
@@ -175,6 +202,32 @@ def str2bool(v):
         return False
     else:
         raise argparse.ArgumentTypeError('Boolean value expected.')
+
+def isFileOfFormat(filePath,fileFormat,throwError=False):
+    #Checks if the Parameter fileFormat is Category and contains multiple fileformats
+    #Iterates threw the Category (list)
+    if isinstance(fileFormat, (tuple, list)):
+        # print fileFormat
+        for singleFileFormat in fileFormat:
+            if os.path.isfile(filePath) and filePath.lower().endswith(str(singleFileFormat)):
+                return True
+            else:
+                if throwError is True:
+                    raise IOError("Couldnt Find File or wasnt a %s File: \"%s\"" % (singleFileFormat, filePath))
+                else:
+                    print "Couldnt Find File or wasnt a %s File: \"%s\"" % (singleFileFormat, filePath)
+        return False
+    else: #executed when the argument fileFormat only contains a single fileformat
+        if os.path.isfile(filePath) and filePath.lower().endswith(str(fileFormat)):
+            return True
+        else:
+            if throwError is True:
+                raise IOError("Couldnt Find File or wasnt a %s File: \"%s\"" % (fileFormat,filePath))
+            else:
+                print "Couldnt Find File or wasnt a %s File: \"%s\"" % (fileFormat,filePath)
+                return False
+
+#=========================Commandline Parser===================================
 #Initialize a arguments parser for commandline based execution of the Script
 parser = argparse.ArgumentParser(prog='AcousticFeatureExtraction',
                                  description='Extracts acoustic features from speech\n',
@@ -215,21 +268,15 @@ parser.add_argument('--version', action='version', version='%(prog)s 1.2\n'
 #TODO: Consider the Creation of a logfile for the script, or look for a library that does it
 #TODO: Add Parser Argument -l for getting a Log File
 #TODO: Add Parser Argument for a Txt File Containing Links to the segmentTxt and the regarding audio
-#TODO: Write a Reader Function for the LinkTxtFile (Paths to segment and audio files)
-#TODO: Add an Argument for command line Based Execution of the Script like -x (execution)
 
 def main(segFiles=None,audFiles=None,execute=True,keep=True,naming=1,therapist=True):
-    AcFtEx = AcousticFeatureExtractor(segFiles,audFiles)
+    AcFtEx = AcousticFeatureExtractor(segFiles,audFiles,therapist)
     AcFtEx.namingOfSlices = naming
     if keep:
         AcFtEx.KEEP = True
     else:
         AcFtEx.KEEP = False
         print "Set constant KEEP to False (Deletion of all temporary files after completion)"
-    if therapist:
-        AcFtEx.parseSequence = "Therapeut\tTherapeut"
-    else:
-        AcFtEx.parseSequence = "Patient\tPatient"
     if execute:
         AcFtEx.executeFtExtraction()
     else:
@@ -237,31 +284,6 @@ def main(segFiles=None,audFiles=None,execute=True,keep=True,naming=1,therapist=T
 
     if not AcFtEx.KEEP:
         AcFtEx.cleanUp()
-
-
-def isFileOfFormat(filePath,fileFormat,throwError=False):
-    #Checks if the Parameter fileFormat is Category and contains multiple fileformats
-    #Iterates threw the Category (list)
-    if isinstance(fileFormat, (tuple, list)):
-        # print fileFormat
-        for singleFileFormat in fileFormat:
-            if os.path.isfile(filePath) and filePath.lower().endswith(str(singleFileFormat)):
-                return True
-            else:
-                if throwError is True:
-                    raise IOError("Couldnt Find File or wasnt a %s File: \"%s\"" % (singleFileFormat, filePath))
-                else:
-                    print "Couldnt Find File or wasnt a %s File: \"%s\"" % (singleFileFormat, filePath)
-        return False
-    else: #executed when the argument fileFormat only contains a single fileformat
-        if os.path.isfile(filePath) and filePath.lower().endswith(str(fileFormat)):
-            return True
-        else:
-            if throwError is True:
-                raise IOError("Couldnt Find File or wasnt a %s File: \"%s\"" % (fileFormat,filePath))
-            else:
-                print "Couldnt Find File or wasnt a %s File: \"%s\"" % (fileFormat,filePath)
-                return False
 
 if __name__ == "__main__":
     args = parser.parse_args() #get the Commandline Arguments
